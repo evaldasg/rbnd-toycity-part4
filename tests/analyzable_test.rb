766bee697d4d1a83a2b83212366d58499326b18e
Evaldas Grudzinskas <prohdo@icloud.com>
2016-07-17 19:10:15 +0300

    Ruby style code fixes based on rubocop

--- tests/analyzable_test.rb
+++ tests/analyzable_test.rb
@@ -6,20 +6,19 @@ require_relative '../data/seeds'
 include Analyzable
 
 class TestAnalyzable < MiniTest::Test
-
   def setup
-    @data_path = File.dirname(__FILE__) + "/../data/data.csv"
-    CSV.open(@data_path, "wb") do |csv|
-      csv << ["id", "brand", "product", "price"]
+    @data_path = File.dirname(__FILE__) + '/../data/data.csv'
+    CSV.open(@data_path, 'wb') do |csv|
+      csv << ['id', 'brand', 'product', 'price']
     end
     db_seed
   end
 
   def test_average_price_method_returns_average_price
-    p1 = Product.create(brand: "AverageBrand", name: "Nyan Cat", price: 15.32)
-    p2 = Product.create(brand: "AverageBrand", name: "Nyan Cat", price: 18.23)
-    p3 = Product.create(brand: "AverageBrand", name: "Nyan Cat", price: 11.30)
-    avg = Analyzable::average_price(Product.where(brand: "AverageBrand"))
+    Product.create(brand: 'AverageBrand', name: 'Nyan Cat', price: 15.32)
+    Product.create(brand: 'AverageBrand', name: 'Nyan Cat', price: 18.23)
+    Product.create(brand: 'AverageBrand', name: 'Nyan Cat', price: 11.30)
+    avg = Analyzable.average_price(Product.where(brand: 'AverageBrand'))
     assert_equal(14.95, avg)
   end
 
